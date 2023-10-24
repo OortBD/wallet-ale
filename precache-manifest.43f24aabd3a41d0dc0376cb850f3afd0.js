50611d8bbcbcac9e56615860c76b4dad96a3983a
OortBD <132399180+OortBD@users.noreply.github.com>
2023-10-24 12:32:04 -0400

    Add files via upload

---
+++ precache-manifest.43f24aabd3a41d0dc0376cb850f3afd0.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7dcf9bae807f58c5e1d8b2fdde1ac21",
+    "url": "/home.html"
+  },
+  {
+    "revision": "3e5322499c701e76f4ab794c4dbba517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac93f4fb4446a81c1317616309cf7d5b",
+    "url": "/notification.html"
+  },
+  {
+    "revision": "51b5e424bac29ad311d5",
+    "url": "/static/css/home.css"
+  },
+  {
+    "revision": "159631b8f514b835aadf",
+    "url": "/static/css/index.css"
+  },
+  {
+    "revision": "3d083f76a6c9b00424e4",
+    "url": "/static/css/notification.css"
+  },
+  {
+    "revision": "ba895fb0dad98c9bacea",
+    "url": "/static/js/background.js"
+  },
+  {
+    "revision": "e63ec0758f57330d1dc8ef9449133495",
+    "url": "/static/js/background.js.LICENSE.txt"
+  },
+  {
+    "revision": "084b2c266f0fdf10242d",
+    "url": "/static/js/content.js"
+  },
+  {
+    "revision": "51b5e424bac29ad311d5",
+    "url": "/static/js/home.js"
+  },
+  {
+    "revision": "4b8cf3e9ed64c7e80f2407095dbee8ba",
+    "url": "/static/js/home.js.LICENSE.txt"
+  },
+  {
+    "revision": "159631b8f514b835aadf",
+    "url": "/static/js/index.js"
+  },
+  {
+    "revision": "4b8cf3e9ed64c7e80f2407095dbee8ba",
+    "url": "/static/js/index.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d083f76a6c9b00424e4",
+    "url": "/static/js/notification.js"
+  },
+  {
+    "revision": "4b8cf3e9ed64c7e80f2407095dbee8ba",
+    "url": "/static/js/notification.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/static/media/Questrial-Regular.37c8a1aa.ttf"
+  },
+  {
+    "revision": "301fe70f8f0f41c236317504ec05f820",
+    "url": "/static/media/Roboto-Black.301fe70f.ttf"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "/static/media/Roboto-Medium.d0884059.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "4f0b85f5b601a405bdc7b23aad6d2a47",
+    "url": "/static/media/Roboto-Thin.4f0b85f5.ttf"
+  },
+  {
+    "revision": "537d299a2fedf1a7541940cdbfa610d9",
+    "url": "/static/media/RussoOne-Regular.537d299a.ttf"
+  },
+  {
+    "revision": "4b83fe8928884526331086d7b340fef6",
+    "url": "/static/media/recoveryKey.4b83fe89.png"
+  },
+  {
+    "revision": "f9901ad3556d8838a07cc80d5945f5e3",
+    "url": "/static/media/updated.f9901ad3.png"
+  }
+]);
